42f55d277928c140ff3543a647a663464630cae1
Himanshu Sahu <himanshu.sahu06@ad.infosys.com>
2023-05-20 10:40:58 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyAWtM7V5DHdYS3JTOMp7SNsm5Bch_c5O6Q",
+    authDomain: "fir-76355.firebaseapp.com",
+    projectId: "fir-76355",
+    storageBucket: "fir-76355.appspot.com",
+    messagingSenderId: "636722790943",
+    appId: "1:636722790943:web:d91e0bf7d25645e8140b2a"
+  }
 };
